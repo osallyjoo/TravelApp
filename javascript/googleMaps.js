6c31bf8d79686d8b8fcae79fd8754cd32cffd919
Mikkal24 <voltx180@gmail.com>
2017-03-08 13:46:30 -0800

    Added google maps to overview.html
    *Please note* that the search function is not working yet googleMaps.js only uses a static API call

--- javascript/googleMaps.js
+++ javascript/googleMaps.js
@@ -1,8 +1,9 @@
 
-function initMap() {
-  var uluru = {lat: -25.363, lng: 131.044};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
+function initMap(cityLat, cityLong) {
+  var uluru = {lat: -25.363, lng: 131.044}; //need to look to see if we can search by city name instead of coordinates
+  var map = new google.maps.Map(document.getElementById('map'), { 
+  	//*NOTE* google.maps.Map doesnt work with jQuery I guess
+    zoom: 8,
     center: uluru
   });
   var marker = new google.maps.Marker({
