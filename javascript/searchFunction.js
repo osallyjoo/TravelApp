92dd5e77b298e3f1d33b7f7e5255617d56abe775
Grant Evans <grant.wallace.evans@gmail.com>
2017-03-08 16:10:39 -0800

    added search functionality, removed links on nav, added firebase

---
+++ javascript/searchFunction.js
@@ -0,0 +1,28 @@
+// connection to firebase
+var config = {
+    apiKey: "AIzaSyBEEv7CBXzPa2wgm2AVAynA6pJ0DC_kovs",
+    authDomain: "trippy-c8caf.firebaseapp.com",
+    databaseURL: "https://trippy-c8caf.firebaseio.com",
+    storageBucket: "trippy-c8caf.appspot.com",
+    messagingSenderId: "964170356211"
+};
+// initialze App
+firebase.initializeApp(config);
+// create shorter version to reference firebase db
+var db = firebase.database();
+
+// when the search button is clicked, do something
+$("#searchBtn").on("click",function(){
+	// grab the text in the search term box
+	var searchTerm = $("#searchTerm").val().trim();
+	// push search to database
+	db.ref().push(searchTerm);
+	// update list on html page
+	$("#recentSearches").append($("<div>").html(searchTerm));
+	// need to call google maps API
+	//callGoogle(searchTerm);
+	// call instagram API
+	//callInstagram(searchTerm);
+	// call weather API
+	//callWeather(searchTerm);
+});
