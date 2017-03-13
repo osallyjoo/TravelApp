bb12260ce43ba72f7e43daafeef210fbbf130342
Jenny Chu <jennychu.sd@gmail.com>
2017-03-12 20:08:33 -0700

    minor edits to weather and tab hover css

--- javascript/weather.js
+++ javascript/weather.js
@@ -5,24 +5,17 @@ function callWeather(searchTerm){
     unit: 'f',
     success: function(weather) {
     var weatherContent = $("<div>")
-    	.append($('<p class="weatherCity">').html(weather.city + ', ' + weather.region))
     	.append($('<p class="currentTemp">').html(weather.temp+'&deg;'+weather.units.temp+ ' / '+weather.alt.temp+'&deg;C'))
         .append($('<p class="currentCond">').html(weather.currently));
 
       var forecastHolder = $('<div>').addClass("weekForecast");
-      console.log(forecastHolder);
       for (var i=0; i < 7; i++) {
         var dayDiv = $("<div class='dayForecast'>")
           .append($("<p>").html(weather.forecast[i].day))
-          .append($("<p>").html(weather.forecast[i].high));
+          .append($("<p>").html(weather.forecast[i].high+'&deg;'+weather.units.temp));
         forecastHolder.append(dayDiv);
       };
       weatherContent.append(forecastHolder);
-      
-      // working code
-      // for(var i=0; i < weather.forecast.length; i++) {
-      //   html += '<p>'+weather.forecast[i].day+': '+weather.forecast[i].high+'</p>';
-      // }
 
       $("#weatherBox").html(weatherContent);
     },
