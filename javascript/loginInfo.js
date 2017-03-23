3d75ce0cf056b3d6e186bbfca44295b02bc53e13
Grant Evans <grant.wallace.evans@gmail.com>
2017-03-22 23:07:10 -0700

    added statechanged function

--- javascript/loginInfo.js
+++ javascript/loginInfo.js
@@ -1,8 +1,8 @@
-$("#loginBtn").on("click",function(){
+$("#loginBtn").on("click", function () {
     // login stuff here
     var email = $("#userNameTerm").val().trim();
     var password = $("#passwordTerm").val().trim();
-    firebase.auth().signInWithEmailAndPassword(email,password).catch(function(error){
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
         var errorCode = error.code;
         var errorMessage = error.Message;
     });
@@ -11,12 +11,12 @@ $("#loginBtn").on("click",function(){
     console.log("You are logged in");
 });
 
-$("#signUpBtn").on("click",function(){
+$("#signUpBtn").on("click", function () {
     // login stuff here
     var email = $("#userNameTerm").val().trim();
     var password = $("#passwordTerm").val().trim();
     console.log(email + "  " + password);
-    firebase.auth().createUserWithEmailAndPassword(email,password).catch(function(error){
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
         var errorCode = error.code;
         var errorMessage = error.Message;
     });
@@ -25,6 +25,17 @@ $("#signUpBtn").on("click",function(){
     console.log("You are signed up");
 });
 
-$("#signOutBtn").on("click",function(){
+$("#signOutBtn").on("click", function () {
     firebase.auth().signOut();
+    $("#usernameDisplay").html("Not Logged In");
+});
+
+$(document).ready(function () {
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+        if (firebaseUser) {
+            $("#usernameDisplay").html(firebaseUser.email);
+        } else {
+            console.log("not logged in");
+        }
+    });
 });
