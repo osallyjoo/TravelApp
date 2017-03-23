81db4472550c6158553007eed29f0f6367742cb5
Grant Evans <grant.wallace.evans@gmail.com>
2017-03-22 22:50:08 -0700

    added sign in and un/pw forms

---
+++ javascript/loginInfo.js
@@ -0,0 +1,30 @@
+$("#loginBtn").on("click",function(){
+    // login stuff here
+    var email = $("#userNameTerm").val().trim();
+    var password = $("#passwordTerm").val().trim();
+    firebase.auth().signInWithEmailAndPassword(email,password).catch(function(error){
+        var errorCode = error.code;
+        var errorMessage = error.Message;
+    });
+    $("#userNameTerm").val("");
+    $("#passwordTerm").val("");
+    console.log("You are logged in");
+});
+
+$("#signUpBtn").on("click",function(){
+    // login stuff here
+    var email = $("#userNameTerm").val().trim();
+    var password = $("#passwordTerm").val().trim();
+    console.log(email + "  " + password);
+    firebase.auth().createUserWithEmailAndPassword(email,password).catch(function(error){
+        var errorCode = error.code;
+        var errorMessage = error.Message;
+    });
+    $("#userNameTerm").val("");
+    $("#passwordTerm").val("");
+    console.log("You are signed up");
+});
+
+$("#signOutBtn").on("click",function(){
+    firebase.auth().signOut();
+});
